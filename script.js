4a05ae1c81b4c29c35a5871204adedad0d12c022
Adolfo Herrera <valherdolf@gmail.com>
2021-03-18 03:38:40 -0400

    Now there is a sound when you click intro button, character selection button, weapon transition selection, when you lose and win, and end button. Also added an icon to the page.

--- script.js
+++ script.js
@@ -16,8 +16,11 @@ const game = () => {
         const charChoosen = document.querySelectorAll(".characterSelection img");
         const charSelection = document.querySelector(".characterSelection");
         const characters = document.querySelector(".characters");
+        const btnSelectSound = document.querySelector('audio[data-sound="btnSelectSound"]');
         
         startBtn.addEventListener("click", () => {
+            btnSelectSound.currentTime = 0;
+            btnSelectSound.play();
             introGame.classList.add("fadeOut");
             charSelection.classList.add("fadeIn");
         })
@@ -26,6 +29,8 @@ const game = () => {
             character.addEventListener("click", function() {
                 let imgCharPlayer = document.getElementById("imgCharPlayer");
                 let imgCharComp = document.getElementById("imgCharComp");
+                btnSelectSound.currentTime = 0;
+                btnSelectSound.play();
 
                 imgCharPlayer.src = this.src;
 
@@ -51,16 +56,20 @@ const game = () => {
         const playerBox = document.querySelector(".sub-b1 img");
         const computerBox = document.querySelector(".sub-b2 img");       
         const computerWeapons = ["rock", "paper", "scissors"];
+        
 
         weapons.forEach(weapon => {
             weapon.addEventListener("click", function() {
                 const computerSelection = computerWeapons[Math.floor(Math.random() * 3)];
                 const boxDotsB1 = document.querySelector(".sub-b1 .threeDots");
                 const boxDotsB2 = document.querySelector(".sub-b2 .threeDots");
+                const afterChoiceSound = document.querySelector('audio[data-sound="afterChoiceSound"]');
 
                 playerSelection = this.dataset.weapon;
                 weaponsContainer.style.pointerEvents = "none";
                 arena.style.animation = "bg-color-trans 0.8s ease-in-out";
+                afterChoiceSound.currentTime = 0;
+                afterChoiceSound.play();
 
                 setTimeout(() => {
                     playerBox.src = `./Images/${this.dataset.weapon}.png`;
@@ -89,6 +98,9 @@ const game = () => {
     const checkLifePoints = () => {
         const div = document.createElement("div");
         const button = document.createElement("button");
+        const loseGameSound = document.querySelector('audio[data-sound="loseGameSound"]');
+        const winGameSound = document.querySelector('audio[data-sound="winGameSound"]');
+        const endBtnSound = document.querySelector('audio[data-sound="endButtonSound"]');
 
         if ((playerScore === 0) || (computerScore === 5)){
             body.insertBefore(div, introGame);
@@ -97,23 +109,38 @@ const game = () => {
             button.classList.add("endBtn");
             mainContent.style.zIndex = "-1";
             mainContent.style.opacity = "0.1";
-            button.onclick = () => { location.reload(); }
 
+            button.onclick = () => { 
+                endBtnSound.currentTime = 0;
+                endBtnSound.play();
+                setTimeout(() => {
+                    location.reload();
+                }, 500);
+                
+            }
             if (playerScore === 0) { 
+                loseGameSound.currentTime = 0;
+                loseGameSound.play();
                 button.textContent = "You Lost!";
                 button.style.backgroundColor = "#8c0101";
             }
             if (computerScore === 5) {
+                winGameSound.currentTime = 0;
+                winGameSound.play();
                 button.textContent = "You Won!";
                 button.style.backgroundColor = "#208502";
-            }
-            
+            }  
         }
     }
 
-    const compareSelections = (playerSelection, computerSelection) => {  
-        
+    const compareSelections = (playerSelection, computerSelection) => { 
+        const drawSound = document.querySelector('audio[data-sound="drawSound"');
+        const losePointSound = document.querySelector('audio[data-sound="losePointSound"]');  
+        const winPointSound = document.querySelector('audio[data-sound="winPointSound"]');
+
         if (playerSelection === computerSelection) {
+            drawSound.currentTime = 0;
+            drawSound.play();
             return;
         }
         if ((playerSelection === "rock" && computerSelection === "scissors") ||
@@ -124,6 +151,8 @@ const game = () => {
                 computerHealth[computerScore - 1].style.animation = "none";
                 computerHealth[computerScore - 1].style.backgroundImage = "none";
                 computerHealth[computerScore - 1].style.filter = "invert(1)";
+                winPointSound.currentTime = 0;
+                winPointSound.play();
                 checkLifePoints();
                 return;
         }
@@ -135,6 +164,8 @@ const game = () => {
                 playerHealth[playerScore].style.animation = "none";
                 playerHealth[playerScore].style.backgroundImage = "none";
                 playerHealth[playerScore].style.filter = "invert(1)";
+                losePointSound.currentTime = 0;
+                losePointSound.play();
                 checkLifePoints();
                 return;
         }
